8cf047f371c23162d9a6a315b988f7def896c91f
Giulia Zeni <giuli.zeni@gmail.com>
2023-02-23 12:20:37 +0100

    first part

---
+++ app/models/movie.rb
@@ -0,0 +1,9 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :lists
+
+  validates :overview, presence: true
+  validates :title, presence: true
+  validates :title, uniqueness: true
+
+end
